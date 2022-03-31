230e7aa2dbd9906bdf93741e149263501d7a4997
jvskv <97881987+jvskv@users.noreply.github.com>
2022-03-31 10:55:05 +0300

    31.3.2022
    Projektin aloitus

---
+++ Ohjelmistoprojekti 1/Ohjelmistoprojekti-1/src/test/java/Tiimi4/Ohjelmistoprojekti1/Ohjelmistoprojekti1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Tiimi4.Ohjelmistoprojekti1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ohjelmistoprojekti1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
