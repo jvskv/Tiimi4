478071615b8309c2278bdf3efdf438415d7d8e11
Kalle Kinnunen <kalle.kinnunen@myy.haaga-helia.fi>
2022-04-28 11:11:48 +0300

    restController

--- Ohjelmistoprojekti 1/Ohjelmistoprojekti-1/src/main/java/Tiimi4/Ohjelmistoprojekti1/domain/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package Tiimi4.Ohjelmistoprojekti1.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuizRepository extends CrudRepository<Quiz, Long> {
-
-}
--- Ohjelmistoprojekti 1/Ohjelmistoprojekti-1/src/main/java/Tiimi4/Ohjelmistoprojekti1/domain/QuizTwoRepository.java
+++
@@ -1,7 +0,0 @@
-package Tiimi4.Ohjelmistoprojekti1.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuizTwoRepository extends CrudRepository<QuizTwo, Long> {
-
-}
